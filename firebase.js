1bffe53d640a762319e761a702440e8623ec188f
Camilo-Cano-2005 <camicano2005@gmail.com>
2022-05-03 15:50:53 -0500

    gonosocio

---
+++ firebase.js
@@ -0,0 +1,22 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyA_1RxtE1LqDoWc5qltfrbxrERhuQS4cUk",
+    authDomain: "la-tiendita-del-guetto.firebaseapp.com",
+    projectId: "la-tiendita-del-guetto",
+    storageBucket: "la-tiendita-del-guetto.appspot.com",
+    messagingSenderId: "217875731234",
+    appId: "1:217875731234:web:353f660544631ca889cb7b",
+    measurementId: "G-83Q63BE6Y9"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
