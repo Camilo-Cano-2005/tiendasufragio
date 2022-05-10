ba040eba6f54276b6b626614c854d0413fc1d5cc
Camilo-Cano-2005 <camicano2005@gmail.com>
2022-05-10 17:15:27 -0500

    carechimba

--- controlador.js
+++ controlador.js
@@ -1,142 +1,142 @@
 //OBJETOS
 let producto1={
-    nombre:"Buso ",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Boina gris",
+    precio:50000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/Boina.jpg?alt=media&token=6983cd1e-9d3a-4fea-8aed-cbeca0c4e22b"
 }
 
 let producto2={
-    nombre:"producto2",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Buso violeta",
+    precio:75000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/Buso-1.jpg?alt=media&token=97140bd7-5e97-4c9a-8435-d1d276baf8c3"
 }
 
 let producto3={
-    nombre:"producto3",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Buso verde",
+    precio:70000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/Buso-2.jpg?alt=media&token=b1ad2551-dae0-497e-bff1-35487a9242c7"
 }
 
 let producto4={
-    nombre:"producto4",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Buso peludo gris",
+    precio:90000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/Buso-3.jpg?alt=media&token=b0130b9a-6f60-43a5-b724-24beeb0f8677"
 }
 
 let producto5={
-    nombre:"producto5",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Tenis blanco y negro",
+    precio:60000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/Tennis-1.jpg?alt=media&token=f2c277bd-453c-44dc-982e-0c3c7287e748"
 }
 
 let producto6={
-    nombre:"producto6",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Tenis blancos",
+    precio:65000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/Tennis-2.jpg?alt=media&token=69469261-97f7-4a62-8be8-666f0f409e01"
 }
 
 let producto7={
-    nombre:"producto7",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Boxers diferente color X3",
+    precio:30000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/boxers-1.jpg?alt=media&token=fbfac522-8cbb-43b6-8299-5a23ce01df10"
 }
 
 let producto8={
-    nombre:"producto8",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Cachaco negro",
+    precio:200000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/cachaco-1.jpg?alt=media&token=83ed3fbe-354f-4b89-bcd8-c20c8d4c9af6"
 }
 
 let producto9={
-    nombre:"producto9",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Camiseta manga corta beige",
+    precio:30000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/camiseta-1.jpg?alt=media&token=16555bdc-e6cf-4aa0-8e2a-bca9ed182639"
 }
 
 let producto10={
-    nombre:"producto10",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Camiseta manga corta vinotinto",
+    precio: 30000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/camiseta-2.jpg?alt=media&token=596ff838-627a-4529-b641-4d5ad5235722"
 }
 
 let producto11={
-    nombre:"producto11",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Camisilla negra",
+    precio:25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/camiseta-corta-1.jpg?alt=media&token=ecb923b7-3d00-4f40-a820-c74f7cf14ec9"
 }
 
 let producto12={
-    nombre:"producto12",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Camiseta manga corta tipo polo naranga",
+    precio:35000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/camiseta-polo-1.jpg?alt=media&token=023bfb1a-c333-4539-87af-0efe2f94ddbc"
 }
 
 let producto13={
-    nombre:"producto13",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Gorra negra",
+    precio:40000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/gorra-1.jpg?alt=media&token=004123e5-1371-4dc1-866d-c1ebe85ead12"
 }
 
 let producto14={
-    nombre:"producto14",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Gorro negro",
+    precio:35000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/gorro.jpg?alt=media&token=bf4f228f-61e4-4334-9438-1f8583659250"
 }
 
 let producto15={
-    nombre:"producto15",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Bluejeans vaqueros",
+    precio:70000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/jeans-1.jpg?alt=media&token=c2345d94-97c8-43e4-8ed0-f5bd94fe8693"
 }
 
 let producto16={
-    nombre:"producto15",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"X5 pares medias negras",
+    precio: 20000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/medias-1.jpg?alt=media&token=7f55e593-af39-4a61-b728-67cfb9ddea29"
 }
 
 let producto17={
-    nombre:"producto15",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Short pijama celeste",
+    precio:20000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/pijama-1.jpg?alt=media&token=cd4e9796-b9aa-4e33-9e10-5458cc49a308"
 }
 
 let producto18={
-    nombre:"producto15",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Sudadera negra",
+    precio:65000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/sudadera-1.jpg?alt=media&token=1a970c48-ae84-493f-8ae8-4698de37cdf5"
 }
 
 let producto19={
-    nombre:"producto15",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Sudadera deportiva verde claro",
+    precio:60000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/sudadera-2.jpg?alt=media&token=15d8e7be-f28c-47a1-993c-16e0a656163f"
 }
 
-let producto15={
-    nombre:"producto15",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+let producto20={
+    nombre:"Pantaloneta traje de baño diseño playero",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/traje_ba%C3%B1o-1.jpg?alt=media&token=570573df-fcb1-41cf-901a-f6f47d0939e8"
 }
 
 //ARREGLO
